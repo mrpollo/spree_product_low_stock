950522c7c3c74d95858cdb6ec1fe3f63338b3075
Ramón Roche <mrpollo@gmail.com>
2013-05-28 18:00:56 -0700

    stock warning helper

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,10 @@
 Spree::Product.class_eval do
   delegate_belongs_to :master, :stock_threshold
   attr_accessible :stock_threshold
+
+  def stock_warning?
+    return true if !stock_threshold.nil? and on_hand > stock_threshold
+    false
+  end
+
 end
