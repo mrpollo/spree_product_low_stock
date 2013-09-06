787333d6a281df117b30b7edb2a5b7bd42f952e1
Ramón Roche <mrpollo@gmail.com>
2013-09-06 14:16:49 -0700

    fix admin form override to show threshold field

--- app/overrides/add_low_stock_threshold_form.rb
+++ app/overrides/add_low_stock_threshold_form.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path   => "spree/admin/products/_form",
   :name           => "add_low_stock_threshold_form",
-  :insert_after   => 'code[erb-silent]:contains("if Spree::Config[:track_inventory_levels]")',
+  :insert_before   => '#shipping_specs',
   :partial        => "spree/admin/products/low_stock_threshold"
 )
