e8e33423ecc22c0ae458840c6921ca84060833f4
geermc4 <geermc4@gmail.com>
2013-11-28 11:22:44 -0800

    2-1

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,5 @@
 Spree::Product.class_eval do
   delegate_belongs_to :master, :stock_threshold
-  attr_accessible :stock_threshold
 
   def stock_warning?
     return true if !stock_threshold.nil? and stock < stock_threshold
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,8 +1,5 @@
 Spree::Variant.class_eval do
-  attr_accessible :stock_threshold
-
   def stock
     stock_items.collect(&:count_on_hand).inject(:+)
   end
-
 end
